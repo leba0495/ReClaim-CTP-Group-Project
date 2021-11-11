d33a742e09a59bbd5a300bbd76a77a1930971f6e
LuisBcode <ab.luis04@gmail.com>
2021-11-11 17:53:11 -0500

    Add Market Place Page
    1. Create js and css files for market place
    2. Add link in App.js and link to market place
    3. Clean up import statements by separating between importing our pages
    and components

--- client/src/App.js
+++ client/src/App.js
@@ -6,18 +6,23 @@ import {
   Link,
   NavLink
 } from 'react-router-dom';
+/* Pages Imports */
 import LandingPage from './pages/LandingPage';
 import PostFormPage from './pages/PostFormPage';
 import ShowPostPage from './pages/ShowPostPage';
 import AboutUsPage from './pages/AboutUsPage';
 import LogInFormPage from './pages/LogInFormPage';
+import SignUpPage from './pages/SignUpPage';
+import MarketPlace from './pages/MarketPlacePage';
+
+/* Components Imports */
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import SignUpPage from './pages/SignUpPage';
+
 import './styles/App.css';
 // import {Navbar, Container, Nav, NavDropdown, Button} from 'react-bootstrap';
 
@@ -41,7 +46,7 @@ function Navigation(props) {
     <Nav className = "ms-auto" >
     <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
         <NavDropdown.Item href="/home">Home</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="/market-place">Market Place</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
         <NavDropdown.Divider />
         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
@@ -71,6 +76,7 @@ class App extends React.Component {
                 <Route path="/about-us" component={AboutUsPage} />
                 <Route path="/log-in" component={LogInFormPage}/>
                 <Route path="/sign-up" component={SignUpPage} />
+                <Route parth="/market-place" component={MarketPlace}/>
                 <Route path="/home" component={LandingPage} />
               </Switch>
             </div>
---
+++ client/src/pages/MarketPlacePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/MarketPlacePage.css'
+
+class Market extends React.Component() {
+    render(){
+        return (
+            <h1>Hello</h1>
+        );
+    }
+}
+
+export default Market;
