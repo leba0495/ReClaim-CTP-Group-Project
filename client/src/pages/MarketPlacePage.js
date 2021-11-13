c67f18548f395af7e44f75b5132c8178d63721ca
LuisBcode <ab.luis04@gmail.com>
2021-11-12 21:33:25 -0500

    Add search bard to market place page

--- client/src/pages/MarketPlacePage.js
+++ client/src/pages/MarketPlacePage.js
@@ -1,11 +1,30 @@
 import React from "react";
-import '../styles/MarketPlacePage.css'
+import {Card} from 'react-bootstrap';
+import '../styles/MarketPlacePage.css';
+
+function SearchBar(props){
+    return (
+        <div>
+            <input 
+            type="text"
+            placeholder="Look up a city..."
+            className="search-bar"
+            results="0"
+            >
+            </input>
+            <button className="search-btn">Search</button>
+        </div>
+    ); 
+}
+
+
 
 class MarketPlacePage extends React.Component {
     render(){
         return (
             <div className="market-main">
-                <h1>Hello</h1>
+            <h1>Hello</h1>
+            <SearchBar/>
             </div>
         );
     }
