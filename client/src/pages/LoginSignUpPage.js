3be3be9989195a64ad14e3474b321f06df597cff
Kessler Lock <kesslerlock@gmail.com>
2021-12-12 19:07:44 -0500

    update login and sign up page, fix minor text error

--- client/src/pages/LoginSignUpPage.js
+++ client/src/pages/LoginSignUpPage.js
@@ -14,10 +14,10 @@ function LoginSignUpPage(props) {
             <Tabs defaultActiveKey="login" id="uncontrolled-tab-example" className="mb-3">
                 
                 <Tab eventKey="login" title="Login">
-                  <Form className = "text-md-left">
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form className = "text-md-left tabStyle">
+                    <Form.Group className="mb-3 " controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control type="email" placeholder="Enter email" className = "formStyle"/>
                         <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                         </Form.Text>
@@ -25,7 +25,7 @@ function LoginSignUpPage(props) {
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="password" placeholder="Password" className = "formStyle" />
                     </Form.Group>
 
                     <Button variant="primary" type="submit" href = "/market-place">
@@ -36,43 +36,43 @@ function LoginSignUpPage(props) {
                 </Tab>
                 {/* sign up tab*/}
                 <Tab eventKey="sign-up" title="Sign up">
-                <Form className = "text-md-left">
+                <Form className = "text-md-left tabStyle">
                     <Row className="mb-3">
                         <Form.Group as={Col} controlId="formGridEmail">
                         <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control type="email" placeholder="Enter email" className = "formStyle"/>
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="password" placeholder="Password" className = "formStyle" />
                         </Form.Group>
                     </Row>
 
                     <Form.Group className="mb-3" controlId="formGridAddress1">
                         <Form.Label>Address</Form.Label>
-                        <Form.Control placeholder="1234 Main St" />
+                        <Form.Control placeholder="1234 Main St" className = "formStyle" />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formGridAddress2">
                         <Form.Label>Address 2</Form.Label>
-                        <Form.Control placeholder="Apartment, studio, or floor" />
+                        <Form.Control placeholder="Apartment, studio, or floor" className = "formStyle" />
                     </Form.Group>
 
                     <Row className="mb-3">
                         <Form.Group as={Col} controlId="formGridCity">
                         <Form.Label>City</Form.Label>
-                        <Form.Control placeholder="ex. Queens" />
+                        <Form.Control placeholder="ex. Queens" className = "formStyle" />
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridCity">
                         <Form.Label>State</Form.Label>
-                        <Form.Control placeholder="ex. NY" />
+                        <Form.Control placeholder="ex. NY" className = "formStyle" />
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridZip">
                         <Form.Label>Zip</Form.Label>
-                        <Form.Control placeholder="ex. 11365" />
+                        <Form.Control placeholder="ex. 11365" className = "formStyle" />
                         </Form.Group>
                     </Row>
 
